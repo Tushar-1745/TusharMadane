54933e30ea0dde35400bf60a00f6dfdb3cbae139
Tushar-1745 <tusharmadane729@gmail.com>
2024-01-26 23:24:23 +0530

    bg

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -10,7 +10,7 @@ const introContainerStyle = {
   flexDirection: 'row',
   alignItems: 'flex-start', // Align items at the start of the cross-axis (vertically)
   justifyContent: 'center', // Center horizontally
-  height: '100vh', // Set height to full viewport height
+  height: '80vh', // Set height to full viewport height
 };
 
 const introContentStyle = {
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
 
 const Education = () => {
+
+  const pStyle={
+    marginBottom: '0px',
+    fontSize: '20px'
+  }
+  const EduHeading={
+    alignItems: 'center', textDecoration: 'underline'
+  }
+  const EduBox={
+    border: '4px solid black', height: '350px', width: '420px'
+  }
+  const EduDetailBox={
+    display:'flex', flexDirection:'column', alignItems:'flex-start', marginLeft:'20px', marginTop:'-11px'
+  }
   return (
     <div>
-      <h1 style={{ fontSize: '60px', fontStyle: 'italic', textAlign: 'center' }}>Education</h1>
+      <h1 style={{fontSize:'60px', fontStyle: 'italic', textAlign: 'center'}}>Education</h1>
 
       <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-
-        <div style={{border: '4px solid black', height: '300px', width: '400px'}}>
-          <h2 style={{alignItems: 'center', textDecoration: 'underline'}}>Primary Education</h2>
-          <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', marginLeft:'20px', marginTop:'-11px'}}>
-            <p style={{ marginBottom: '0px' }}><strong>Name: </strong>Shaskiya Vidyaniketan, Pusegaon</p>
-            <p style={{ marginBottom: '0px' }}><strong>Address: </strong>Pusegaon, Tel-Khatav, Dist-Satara</p>
-            <p><strong>SSC Percentage: </strong>97</p>
+        <div style={EduBox}>
+          <h2 style={EduHeading}>Primary Education</h2>
+          <div style={EduDetailBox}>
+            <p style={pStyle}><strong>Name: </strong>Shaskiya Vidyaniketan, Pusegaon</p>
+            <p style={pStyle}><strong>Address: </strong>Pusegaon, Tel-Khatav, Dist-Satara</p>
+            <p style={pStyle}><strong>SSC Percentage: </strong>97</p>
           </div>
         </div>
 
-        <div style={{border: '4px solid black', height: '300px', width: '400px'}}>
-          <h2 style={{alignItems: 'center', textDecoration: 'underline'}}> Junior College</h2>
-          <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', marginLeft:'20px', marginTop:'-11px'}}>
-            <p style={{ marginBottom: '0px' }}><strong>Name: </strong>Laxmanrao Apte Jr. College</p>
-            <p style={{ marginBottom: '0px' }}><strong>Address: </strong>Apte road, Deccan Gymkhana, Pune-411004</p>
-            <p><strong>Course:</strong>12th Science Stream</p>
-            <p><strong>HSC Percentage: </strong>73.23</p>
-            <p><strong>Course Duration: </strong>2018-2020</p>
+        <div style={EduBox}>
+          <h2 style={EduHeading}> Junior College</h2>
+          <div style={EduDetailBox}>
+            <p style={pStyle}><strong>Name: </strong>Laxmanrao Apte Jr. College</p>
+            <p style={pStyle}><strong>Address: </strong>Deccan Gymkhana, Pune-411004</p>
+            <p style={pStyle}><strong>Course:</strong>12th Science Stream</p>
+            <p style={pStyle}><strong>HSC Percentage: </strong>73.23</p>
+            <p style={pStyle}><strong>Course Duration: </strong>2018-2020</p>
           </div>
         </div>
-        
-        <div style={{border: '4px solid black', height: '300px', width: '400px'}}>
-          <h2 style={{alignItems: 'center', textDecoration: 'underline'}}> Senior College</h2>
-          <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', marginLeft:'20px', marginTop:'-11px'}}>
-            <p style={{ marginBottom: '0px' }}><strong>Name: </strong>Sinhgad Institute of Technology and Science</p>
-            <p style={{ marginBottom: '0px' }}><strong>Address: </strong>Narhegaon, Ambegaon, Pune</p>
-            <p><strong>Course:</strong>Bachelor Degree in Computer Science and Engineering</p>
-            <p><strong>CGPA: </strong>73.23</p>
-            <p><strong>Course Duration: </strong>2020-2024</p>
-          </div>
+
+      <div style={EduBox}>
+        <h2 style={EduHeading}>Senior College</h2>
+        <div style={EduDetailBox}>
+          <p style={{ ...pStyle, marginLeft:'-15px' }}><strong>Name: </strong>Sinhgad Institute of Technology and Science</p>
+          <p style={pStyle}><strong>Address: </strong>Narhegaon, Ambegaon, Pune</p>
+          <p style={pStyle}><strong>Course:</strong> Computer Science and Engineering</p>
+          <p style={pStyle}><strong>CGPA: </strong>73.23</p>
+          <p style={pStyle}><strong>Course Duration: </strong>2020-2024</p>
         </div>
+      </div>
+
       </div>
     </div>
   );
 };
 
 export default Education;
+
